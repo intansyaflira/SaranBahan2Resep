aec19069a973057d89ebb6bf7748428985828119
MOKLET-1 <MOKLET-1@LAPTOP-0NAD4MMP>
2020-11-04 12:07:13 +0700

    Initial Commit

---
+++ src/saranbahan2resep/SaranBahan2Resep.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saranbahan2resep;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class SaranBahan2Resep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String identitas = "Intan Dwi Syaflira / XR4 / 19";
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.print ("\nSaran Resep dari Bahan Milik Anda\n");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bahan Pertama: ");
+        System.out.println("1. Pisang");
+        System.out.println("2. Telur ");
+        System.out.print("Masukkan no pilihan Anda :");
+        int bahan1 = scanner.nextInt();
+        
+    }
+    
+}
