0ec038222948f22cb55586621c602f32de0230f8
MOKLET-1 <MOKLET-1@LAPTOP-0NAD4MMP>
2020-11-04 12:19:21 +0700

    TambahInputBahan2

--- src/saranbahan2resep/SaranBahan2Resep.java
+++ src/saranbahan2resep/SaranBahan2Resep.java
@@ -34,6 +34,22 @@ public class SaranBahan2Resep {
         System.out.print("Masukkan no pilihan Anda :");
         int bahan1 = scanner.nextInt();
         
+        if (bahan1 == 1 || bahan1 == 2){
+            System.out.println("Bahan Kedua");
+            if(bahan1 == 1) {
+                System.out.println("1. Susu");
+                System.out.println("2. Minyak Goreng");
+                System.out.println("3. Tidak ada");
+            }else{
+                System.out.println("1. Minyak Goreng");
+                System.out.println("2. Roti");
+                System.out.println("3. Tidak ada");
+            }
+            System.out.println("Masukkan no pilihan anda");
+            int bahan2 = scanner.nextInt();
+            
+        }else
+            System.out.println("Mohon Maaf, Pilihan tidak ditemukan, " + "tidak dapat memberikan saran resep");
     }
     
 }
